989582cd6c2d618f58c40571ba1eee911b6479c7
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 12:07:04 +0100

    Ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            string str2;
+            double num1;
+            double num2;
+            double result;
+
+            Console.WriteLine("Write your first real number:");
+            str1 = Console.ReadLine();
+            num1 = Convert.ToDouble(str1);
+            Console.WriteLine("Write your second real number:");
+            str2 = Console.ReadLine();
+            num2 = Convert.ToDouble(str2);
+            result = Math.Pow(num1,num2);
+            Console.WriteLine($"{str1} elevated to the power of {str2} is: {result}");
+        }
+    }
+}
