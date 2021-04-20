ab2e337900173f26d6a238ec9d65c80fbe0842cf
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 10:24:33 +0100

    Ex11

---
+++ Ex11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter with your string");
+            string userChoice = Console.ReadLine();
+            char[] arr = userChoice.ToCharArray();
+            Array.Reverse(arr);
+            Console.WriteLine(arr);
+        }
+    }
+}
