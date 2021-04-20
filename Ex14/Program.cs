5e09a1f97bbfd6371542e84be91595e6ab0c5c07
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 12:14:24 +0100

    Ex14

---
+++ Ex14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_lenght;
+            string str_height;
+            string str_width;
+            double lenght;
+            double height;
+            double width;
+            double result;
+
+            Console.WriteLine("Write the lenght:");
+            str_lenght = Console.ReadLine();
+            lenght = Convert.ToDouble(str_lenght);
+            Console.WriteLine("Write the height:");
+            str_height = Console.ReadLine();
+            height = Convert.ToDouble(str_height);
+            Console.WriteLine("Write width:");
+            str_width = Console.ReadLine();
+            width = Convert.ToDouble(str_width);
+
+            result = lenght * width * height;
+
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
