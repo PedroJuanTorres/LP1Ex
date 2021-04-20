39dbeca3020ac55f4cc17ab9ac921506e3aa7fed
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 12:19:10 +0100

    Ex15, dont know how it works

---
+++ Ex15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Length property provides the number of array element
+            System.Console.WriteLine("parameter count = {0}", args.Length);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+	            System.Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+        }
+    }
+}
