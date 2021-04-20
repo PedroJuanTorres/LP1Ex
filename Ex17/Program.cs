3eaa54312a450b6c6ea1273241da5272b6f911d0
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 13:10:52 +0100

    Ex17

---
+++ Ex17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int num;
+
+            Console.WriteLine("Write your number:");
+            str = Console.ReadLine();
+            num = Convert.ToInt32(str);
+
+            if(num%5==0 && num%3==0){
+                Console.WriteLine("FizzBuzz!");
+            }
+            else if(num%5==0){
+                Console.WriteLine("Buzz!");
+            }
+            else if(num%3==0){
+                Console.WriteLine("Fizz!");
+            }
+            else{
+                Console.WriteLine($"{num}!");
+            }
+        }
+    }
+}
