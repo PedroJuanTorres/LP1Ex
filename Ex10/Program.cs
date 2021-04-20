0f23f7abdacce43e5d72910c188e9620a1cb9f7e
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 10:16:28 +0100

    Ex 10

---
+++ Ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter with your string");
+            string userChoice = Console.ReadLine();
+            Console.WriteLine($"Your string is: {userChoice}");
+        }
+    }
+}
