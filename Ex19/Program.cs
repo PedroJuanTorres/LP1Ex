6db4e7fc5e3f40858a8e6d4e084089c3dc4b9340
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 13:38:55 +0100

    Ex19

---
+++ Ex19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1,str2;
+            double highScore, newScore;
+
+            Console.WriteLine("Write the High score:");
+            str1 = Console.ReadLine();
+            highScore = Convert.ToDouble(str1);
+            Console.WriteLine("Write the new score:");
+            str2 = Console.ReadLine();
+            newScore = Convert.ToDouble(str2);
+
+            Console.WriteLine(newScore > highScore ? "New high score! " : "Keep trying");
+        }
+    }
+}
