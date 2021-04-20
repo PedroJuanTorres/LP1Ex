fe816468ef078e81a2c3c1e76706030418c5eaac
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-04-20 13:30:14 +0100

    Ex18

---
+++ Ex18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+        
+            Console.WriteLine("Write the meaning of life:");
+            str = Console.ReadLine();
+            
+            if(str.Length<5){
+                Console.WriteLine("That's not very meaningful, is it?");
+            }
+            else if(str.Length>20){
+                Console.WriteLine("Too long to read");
+            }
+            else{
+                Console.WriteLine("Ok, if you say so");
+            }
+        }
+    }
+}
